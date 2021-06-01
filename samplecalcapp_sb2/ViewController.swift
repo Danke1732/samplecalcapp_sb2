58aac9e6d7e6600944cd6273d0a7d41221ef1045
Danke1732 <s0lfa1732@gmail.com>
2021-06-01 15:51:35 +0900

    init

--- samplecalcapp_sb2/ViewController.swift
+++ samplecalcapp_sb2/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myLabel.textColor = UIColor.green
+        myLabel.font = UIFont.systemFont(ofSize: 20.0, weight: UIFont.Weight.bold)
     }
-
-
+    
+    @IBAction func onTap(_ sender: UIButton) {
+        print("onTap!")
+        self.performSegue(withIdentifier: "toPage2", sender: nil)
+    }
+    
+    @IBOutlet weak var myTextField: UITextField!
+    
+    @IBAction func onChange(_ sender: UITextField) {
+        // TextFieldから文字を取得
+        let textFieldString = myTextField.text!
+        print(textFieldString)
+        //myTextField.
+    }
+    
+    @IBAction func onValueChange(_ sender: UITextField) {
+        
+        //print("aa")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "toPage2" {
+            let vc = segue.destination as? Page2ViewController
+            vc?.attrInt = 1
+        }
+        
+        
+//
+//        if let vc = segue.identifier as? Page2ViewController {
+//            vc.attrInt = 1
+//        }
+//        if segue.identifier == "toPage2" {
+//            let vc = segue.description as! Page2ViewController
+//
+//        }
+    }
 }
 
